a29d658dd8d24064816dc08e169c8dec91408d8c
Gilberto Mascena <gilbertomascena@gmail.com>
2026-03-20 09:25:24 -0300

    Setup inicial: Spring 3.4.3, Postgres e Docker

---
+++ src/main/java/br/com/gilbertodev/apipetshop/ApiPetshopApplication.java
@@ -0,0 +1,13 @@
+package br.com.gilbertodev.apipetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPetshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPetshopApplication.class, args);
+    }
+
+}
