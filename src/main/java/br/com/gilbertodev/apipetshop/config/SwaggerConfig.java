5d305ffe73b7d458fbbcb52d582122c2bd41035e
Gilberto Mascena <gilbertomascena@gmail.com>
2026-03-22 22:56:59 -0300

    feat(config): setup Swagger OpenAPI e CORS mappings

---
+++ src/main/java/br/com/gilbertodev/apipetshop/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package br.com.gilbertodev.apipetshop.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Pet Shop - Sistema de Gerenciamento")
+                        .version("1.0")
+                        .description("API para controle de pets, clientes e agendamentos de banho e tosa.")
+                        .contact(new Contact()
+                                .name("Gilbert | Dev")
+                                .email("gilbertomascena@gmail.com")
+                                .url("https://github.com/Gilberto-Mascena")));
+    }
+}
---
+++ src/main/java/br/com/gilbertodev/apipetshop/config/WebConfig.java
@@ -0,0 +1,16 @@
+package br.com.gilbertodev.apipetshop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Libera todos os endpoints
+                .allowedOrigins("*") // Link front
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+}
