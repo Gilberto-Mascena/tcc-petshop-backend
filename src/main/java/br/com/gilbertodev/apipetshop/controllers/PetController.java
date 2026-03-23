8195f724950cc918ed9af43acd3beaea6744d4a8
Gilberto Mascena <gilbertomascena@gmail.com>
2026-03-22 22:17:16 -0300

    feat: implementacao do PetController com endpoints CRUD e validacao

---
+++ src/main/java/br/com/gilbertodev/apipetshop/controllers/PetController.java
@@ -0,0 +1,49 @@
+package br.com.gilbertodev.apipetshop.controllers;
+
+import br.com.gilbertodev.apipetshop.dtos.PetRequestDTO;
+import br.com.gilbertodev.apipetshop.dtos.PetResponseDTO;
+import br.com.gilbertodev.apipetshop.services.PetService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/pets")
+public class PetController {
+
+    private final PetService petService;
+
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PetResponseDTO> salvar(@RequestBody @Valid PetRequestDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(petService.salvar(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PetResponseDTO>> buscarTodos() {
+        return ResponseEntity.ok(petService.listarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PetResponseDTO> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(petService.buscarPorId(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PetResponseDTO> atualizar(@PathVariable Long id, @RequestBody @Valid PetRequestDTO dto) {
+        return ResponseEntity.ok(petService.atualizar(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        petService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
