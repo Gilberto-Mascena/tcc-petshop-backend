31d1226f9eddd37a6089a25f60bba1cbdc018778
Gilberto Mascena <gilbertomascena@gmail.com>
2026-04-06 09:15:38 -0300

    config: ajuste na leitura de variáveis de ambiente com dotenv

--- src/main/java/br/com/gilbertodev/apipetshop/ApiPetshopApplication.java
+++ src/main/java/br/com/gilbertodev/apipetshop/ApiPetshopApplication.java
@@ -1,5 +1,6 @@
 package br.com.gilbertodev.apipetshop;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,7 +10,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class ApiPetshopApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
         SpringApplication.run(ApiPetshopApplication.class, args);
     }
-
 }
