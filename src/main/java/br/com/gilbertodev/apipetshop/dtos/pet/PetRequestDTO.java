f7cb039992741fcb72e3afbf2f94122374e2103c
Gilberto Mascena <gilbertomascena@gmail.com>
2026-04-13 10:38:33 -0300

    refactor: adicionar validações e método toEntity ao PetRequestDTO

--- src/main/java/br/com/gilbertodev/apipetshop/dtos/pet/PetRequestDTO.java
+++ src/main/java/br/com/gilbertodev/apipetshop/dtos/pet/PetRequestDTO.java
@@ -1,6 +1,7 @@
 package br.com.gilbertodev.apipetshop.dtos.pet;
 
 import br.com.gilbertodev.apipetshop.entities.Pet;
+import br.com.gilbertodev.apipetshop.enums.PortePet;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
@@ -8,7 +9,6 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.BeanUtils;
 
 import java.time.LocalDate;
 
@@ -32,9 +32,19 @@ public class PetRequestDTO {
     private LocalDate dataNascimento;
     private String observacoes;
 
+    @NotNull(message = "O porte do pet é obrigatório")
+    private PortePet porte;
+
     public Pet toEntity() {
         Pet pet = new Pet();
-        BeanUtils.copyProperties(this, pet);
+
+        pet.setNome(this.nome);
+        pet.setEspecie(this.especie);
+        pet.setRaca(this.raca);
+        pet.setDataNascimento(this.dataNascimento);
+        pet.setObservacoes(this.observacoes);
+        pet.setPorte(this.porte);
+
         return pet;
     }
 }
