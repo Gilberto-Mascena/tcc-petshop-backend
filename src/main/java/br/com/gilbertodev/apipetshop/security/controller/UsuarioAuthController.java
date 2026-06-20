b839e9fe1767e04a18fb5fd519cd77376e642daa
Gilberto Mascena <gilbertomascena@gmail.com>
2026-06-20 11:08:31 -0300

    feat: implementar UsuarioAuthController para autenticacao via Spring Security e JWT

--- src/main/java/br/com/gilbertodev/apipetshop/security/controller/UsuarioAuthController.java
+++ src/main/java/br/com/gilbertodev/apipetshop/security/controller/UsuarioAuthController.java
@@ -3,11 +3,8 @@ package br.com.gilbertodev.apipetshop.security.controller;
 import br.com.gilbertodev.apipetshop.doc.UsuarioAuthControllerDoc;
 import br.com.gilbertodev.apipetshop.dtos.autenticacao.UsuarioLoginRequestDTO;
 import br.com.gilbertodev.apipetshop.dtos.autenticacao.UsuarioTokenResponseDTO;
-import br.com.gilbertodev.apipetshop.dtos.usuario.UsuarioRequestDTO;
-import br.com.gilbertodev.apipetshop.dtos.usuario.UsuarioResponseDTO;
 import br.com.gilbertodev.apipetshop.entities.Usuario;
 import br.com.gilbertodev.apipetshop.security.token.TokenService;
-import br.com.gilbertodev.apipetshop.services.UsuarioService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,9 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
 
 @RestController
 @RequestMapping("/auth")
@@ -26,12 +20,12 @@ public class UsuarioAuthController implements UsuarioAuthControllerDoc {
 
     private final AuthenticationManager authenticationManager;
     private final TokenService tokenService;
-    private final UsuarioService usuarioService;
 
-    public UsuarioAuthController(AuthenticationManager authenticationManager, TokenService tokenService, UsuarioService usuarioService) {
+
+    public UsuarioAuthController(AuthenticationManager authenticationManager, TokenService tokenService) {
         this.authenticationManager = authenticationManager;
         this.tokenService = tokenService;
-        this.usuarioService = usuarioService;
+
     }
 
     @Override
@@ -43,16 +37,5 @@ public class UsuarioAuthController implements UsuarioAuthControllerDoc {
         var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
 
         return ResponseEntity.ok(new UsuarioTokenResponseDTO(tokenJWT));
-    }
-
-    @PostMapping("/cadastro")
-    public ResponseEntity<UsuarioResponseDTO> autoCadastro(@RequestBody @Valid UsuarioRequestDTO dados) {
-        UsuarioResponseDTO novoCliente = usuarioService.cadastrarCliente(dados);
-        URI uri = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(novoCliente.id())
-                .toUri();
-        return ResponseEntity.created(uri).body(novoCliente);
     }
 }
