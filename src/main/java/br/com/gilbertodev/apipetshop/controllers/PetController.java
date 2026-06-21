1b985bd1a43cb8746b261326fbd2fb144c57f8ab
Gilberto Mascena <gilbertomascena@gmail.com>
2026-06-21 18:12:12 -0300

    feat: implementar PreAuthorize para controle de acessos no PetController

--- src/main/java/br/com/gilbertodev/apipetshop/controllers/PetController.java
+++ src/main/java/br/com/gilbertodev/apipetshop/controllers/PetController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -26,6 +27,7 @@ public class PetController implements PetControllerDoc {
 
     @Override
     @PostMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'ATENDENTE')")
     public ResponseEntity<PetResponseDTO> salvar(@RequestBody @Valid PetRequestDTO dto) {
         PetResponseDTO response = petService.salvar(dto);
         URI uri = ServletUriComponentsBuilder
@@ -38,30 +40,35 @@ public class PetController implements PetControllerDoc {
 
     @Override
     @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'ATENDENTE')")
     public ResponseEntity<Page<PetResponseDTO>> listarTodos(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
         return ResponseEntity.ok(petService.listarTodos(paginacao));
     }
 
     @Override
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'ATENDENTE')")
     public ResponseEntity<PetResponseDTO> buscarPorId(@PathVariable Long id) {
         return ResponseEntity.ok(petService.buscarPorId(id));
     }
 
     @Override
     @GetMapping("/buscar")
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'ATENDENTE')")
     public ResponseEntity<Page<PetResponseDTO>> buscaGlobal(@RequestParam(required = false) String termo, @PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
         return ResponseEntity.ok(petService.buscaGlobal(termo, paginacao));
     }
 
     @Override
     @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'ATENDENTE')")
     public ResponseEntity<PetResponseDTO> atualizar(@PathVariable Long id, @RequestBody @Valid PetRequestDTO dto) {
         return ResponseEntity.ok(petService.atualizar(id, dto));
     }
 
     @Override
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deletar(@PathVariable Long id) {
         petService.deletar(id);
         return ResponseEntity.noContent().build();
