515fbfb47944ee221b42b38ee034409369b532fb
Gilberto Mascena <gilbertomascena@gmail.com>
2026-06-21 18:09:05 -0300

    feat: proteger endpoints de tutores com PreAuthorize

--- src/main/java/br/com/gilbertodev/apipetshop/controllers/TutorController.java
+++ src/main/java/br/com/gilbertodev/apipetshop/controllers/TutorController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -26,6 +27,7 @@ public class TutorController implements TutorControllerDoc {
 
     @Override
     @PostMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'ATENDENTE')")
     public ResponseEntity<TutorResponseDTO> salvar(@RequestBody @Valid TutorRequestDTO dto) {
         TutorResponseDTO response = tutorService.salvar(dto);
         URI uri = ServletUriComponentsBuilder
@@ -38,30 +40,35 @@ public class TutorController implements TutorControllerDoc {
 
     @Override
     @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'ATENDENTE')")
     public ResponseEntity<Page<TutorResponseDTO>> listarTodos(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
         return ResponseEntity.ok(tutorService.listarTodos(paginacao));
     }
 
     @Override
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'ATENDENTE')")
     public ResponseEntity<TutorResponseDTO> buscarPorId(@PathVariable Long id) {
         return ResponseEntity.ok(tutorService.buscarPorId(id));
     }
 
     @Override
     @GetMapping("/buscar")
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'ATENDENTE')")
     public ResponseEntity<Page<TutorResponseDTO>> buscaGlobal(@RequestParam(required = false) String termo, @PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
         return ResponseEntity.ok(tutorService.buscaGlobal(termo, paginacao));
     }
 
     @Override
     @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'ATENDENTE')")
     public ResponseEntity<TutorResponseDTO> atualizar(@PathVariable Long id, @RequestBody @Valid TutorRequestDTO dto) {
         return ResponseEntity.ok(tutorService.atualizar(id, dto));
     }
 
     @Override
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deletar(@PathVariable Long id) {
         tutorService.deletar(id);
         return ResponseEntity.noContent().build();
