6d5ad595837f8d075d0b9f379b466ad5b267f15b
Gilberto Mascena <gilbertomascena@gmail.com>
2026-06-21 18:41:05 -0300

    feat: restringir acesso total do UsuarioController ao perfil ADMIN

--- src/main/java/br/com/gilbertodev/apipetshop/controllers/UsuarioController.java
+++ src/main/java/br/com/gilbertodev/apipetshop/controllers/UsuarioController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -16,6 +17,7 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("/api/admin/usuarios")
+@PreAuthorize("hasRole('ADMIN')")
 public class UsuarioController implements UsuarioControllerDoc {
 
     private final UsuarioService usuarioService;
